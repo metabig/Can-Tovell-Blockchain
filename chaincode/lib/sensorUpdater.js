3405ef77b5522a5e6a284facc00a6770ec518283
Antoni Bergas Galmés <tonibergas.tb@gmail.com>
2022-02-01 18:44:04 +0100

    Renaming...

--- chaincode/lib/sensorUpdater.js
+++ chaincode/lib/sensorUpdater.js
@@ -14,33 +14,31 @@ const { Contract } = require('fabric-contract-api');
 class SensorUpdater extends Contract {
 
     async InitLedger(ctx) {
-        const assets = [
+        const devices = [
             {
-                ID: 'asset1',
-                Color: 'blue',
-                Size: 5,
-                Owner: 'Tomoko',
-                AppraisedValue: 300,
+                ID: 'sensor1',
+                Unit: 'degree',
+                Value: 15,
             },
         ];
 
-        for (const asset of assets) {
-            asset.docType = 'asset';
+        for (const device of devices) {
+            device.docType = 'sensor';
             // example of how to *write to world state deterministically*
             // use convetion of alphabetic order
             // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
             // when retrieving data, in any lang, the order of data will be the same and consequently also the corresonding hash
-            await ctx.stub.putState(asset.ID, Buffer.from(stringify(sortKeysRecursive(asset))));
+            await ctx.stub.putState(device.ID, Buffer.from(stringify(sortKeysRecursive(device))));
         }
     }
 
     // ReadAsset returns the asset stored in the world state with given id.
     async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
-        if (!assetJSON || assetJSON.length === 0) {
+        const deviceJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+        if (!deviceJSON || deviceJSON.length === 0) {
             throw new Error(`The asset ${id} does not exist`);
         }
-        return assetJSON.toString();
+        return deviceJSON.toString();
     }
 
     // GetAllAssets returns all assets found in the world state.
@@ -62,6 +60,38 @@ class SensorUpdater extends Contract {
             result = await iterator.next();
         }
         return JSON.stringify(allResults);
+    }
+
+    // UpdateAsset updates an existing asset in the world state with provided parameters.
+    async UpdateAsset(ctx, id, unit, value) {
+        const exists = await this.AssetExists(ctx, id);
+        if (!exists) {
+            throw new Error(`The asset ${id} does not exist`);
+        }
+
+        // overwriting original asset with new asset
+        const updatedAsset = {
+            ID: id,
+            Unit: unit,
+            Value: value,
+        };
+        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
+        return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
+    }
+
+    // DeleteAsset deletes an given asset from the world state.
+    async DeleteAsset(ctx, id) {
+        const exists = await this.AssetExists(ctx, id);
+        if (!exists) {
+            throw new Error(`The asset ${id} does not exist`);
+        }
+        return ctx.stub.deleteState(id);
+    }
+
+    // AssetExists returns true when asset with given ID exists in world state.
+    async AssetExists(ctx, id) {
+        const deviceJSON = await ctx.stub.getState(id);
+        return deviceJSON && deviceJSON.length > 0;
     }
 }
 
